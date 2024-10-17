91e6fcbe3e51144bbbf09f64a1f84a8b9d8189e0
Christian Zufferey <christian@zufferey.com>
2024-10-17 11:39:17 +0200

    wip: ça compile mais pas encore eu le temps de tester sur le hardware !

--- esp32-c3-1-wire-usb.ino
+++ esp32-c3-1-wire-usb.ino
@@ -3,9 +3,10 @@
 //
 // ATTENTION, ce code a été testé sur un esp32-c3 super mini. Pas testé sur les autres boards !
 //
-#define zVERSION        "zf241017.1106"
+#define zVERSION        "zf241017.1136"
+#define zHOST           "esp32-c3-1-wire-usb"     // ATTENTION, tout en minuscule
 
-int zDelay1Interval =   30000;                  // Délais en mili secondes pour la boucle loop
+int zDelay1Interval =   30000;                    // Délais en mili secondes pour la boucle loop
 
 /*
 Utilisation:
@@ -54,35 +55,11 @@ const int buttonPin = 9;          // the number of the pushbutton pin
 #include "zSonarpulse.h"
 
 
-// WIFI
-#include "zWifi.h"
-
-
-// OTA WEB server
-#include "otaWebServer.h"
-
-
-// MQTT
-#include "zMqtt.h"
-
-
 // Temperature sensor
-#include "zTemperature.h"
-
-#if zDSLEEP == 1
-  // Deep Sleep
-  #define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
-  // #define TIME_TO_SLEEP  300      /* Time ESP32 will go to sleep (in seconds) */
-  RTC_DATA_ATTR int bootCount = 0;
-#endif
+#include "z1-Wire.h"
 
 
 void setup() {
-  // Il faut lire la température tout de suite au début avant que le MCU ne puisse chauffer !
-  initHTU21DSensor();
-  delay(200);
-  readSensor();
-
   // Pulse deux fois pour dire que l'on démarre
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW); delay(zSonarPulseOn); digitalWrite(ledPin, HIGH); delay(zSonarPulseOff);
@@ -95,85 +72,21 @@ void setup() {
   delay(3000);                          //le temps de passer sur la Serial Monitor ;-)
   Serial.println("\n\n\n\n**************************************\nCa commence !"); Serial.println(zHOST ", " zVERSION);
 
-  #if zDSLEEP == 1
-    //Increment boot number and print it every reboot
-    ++bootCount;
-    sensorValue4 = bootCount;
-    Serial.println("Boot number: " + String(bootCount));
-    // Configuration du dsleep
-    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
-    Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) + " Seconds");
-  #endif
-
-  // Start WIFI
-  zStartWifi();
-  sensorValue3 = WiFi.RSSI();
-
-  // Start OTA server
-  otaWebServer();
-
-  // Connexion au MQTT
-  Serial.println("\n\nConnect MQTT !\n");
-  ConnectMQTT();
-
-  Serial.print("http://");
-  Serial.print(zHOST);
-  Serial.println(".local");
+  // Configure les sondes de température 1-wire DS18B20
+  initDS18B20Sensor();
 
   // go go go
   Serial.println("\nC'est parti !\n");
-
-  // Envoie toute la sauce !
-  zEnvoieTouteLaSauce();
-  Serial.println("\nC'est envoyé !\n");
-
-  #if zDSLEEP == 1
-    // Partie dsleep. On va dormir !
-    Serial.println("Going to sleep now");
-    delay(200);
-    Serial.flush(); 
-    esp_deep_sleep_start();
-    Serial.println("This will never be printed");
-  #endif
-
 }
+
 
 void loop() {
   // Envoie toute la sauce !
-  zEnvoieTouteLaSauce();
-
-  // Délais non bloquant pour le sonarpulse et l'OTA
-  zDelay1(zDelay1Interval);
-}
-
-
-// Envoie toute la sauce !
-void zEnvoieTouteLaSauce(){
-
   // Lit les températures
   readSensor();
 
-  // Envoie les mesures au MQTT
-  sendSensorMqtt();
-
-  // Graphe sur l'Arduino IDE les courbes des mesures
-  Serial.print("sensor1:");
-  Serial.print(sensorValue1);
-  Serial.print(",tempInternal1:");
-  Serial.print(tempInternal1);
-  Serial.print(",tempInternal2:");
-  Serial.print(tempInternal2);
-
-  Serial.print(",temp_HTU21D:");
-  Serial.print(sensorValue5);
-  Serial.print(",hum_HTU21D:");
-  Serial.print(sensorValue2);
-
-  // Serial.print(",sensor3:");
-  // Serial.print(sensorValue3);
-  // Serial.print(",sensor4:");
-  // Serial.print(sensorValue4);
-  Serial.println("");
+  // Délais non bloquant pour le sonarpulse et l'OTA
+  zDelay1(zDelay1Interval);
 }
 
 
@@ -181,8 +94,6 @@ void zEnvoieTouteLaSauce(){
 void zDelay1(long zDelayMili){
   long zDelay1NextMillis = zDelayMili + millis(); 
   while(millis() < zDelay1NextMillis ){
-    // OTA loop
-    server.handleClient();
     // Un petit coup sonar pulse sur la LED pour dire que tout fonctionne bien
     sonarPulse();
   }
--- z1-Wire.h
+++ z1-Wire.h
@@ -13,51 +13,9 @@ const int vccPin = 1;       // the number of the VCC pin
 const int pullupPin = 2;    // the number of the PULLUP pin
 const int oneWireBus = 3;   // GPIO where the DS18B20 is connected to
 const int gndPin = 4;       // the number of the GND pin
-// Setup a oneWire instance to communicate with any OneWire devices
-OneWire oneWire(oneWireBus);
-// Pass our oneWire reference to Dallas Temperature sensor 
-DallasTemperature sensors(&oneWire);
-
-
-// Temperature sensor DS18B20 initialising
-void initDS18B20Sensor(){
-    pinMode(gndPin, OUTPUT);   // gnd
-    digitalWrite(gndPin, LOW);
-    pinMode(pullupPin, INPUT_PULLUP);   // pull up
-    pinMode(vccPin, OUTPUT );   // vcc
-    digitalWrite(vccPin, HIGH);
-    // Start the DS18B20 sensor
-    sensors.begin();
-}
-
-
-RTC_DATA_ATTR float tempInternal1 = 0;
-RTC_DATA_ATTR float tempInternal2 = 0;
-
-
-// Lit les senseurs
-void readSensor(){
-    // lit la température interne
-    sensorValue1 = temperatureRead();
-    sensorValue1 = sensorValue1 - 8.0;        // Enlève des ° en trop, je ne sais pas pourquoi ? zf240526.1142, zf240530.0908
-
-    // moyenne glissante
-    sensorValue1 = (sensorValue1 + tempInternal1 + tempInternal2) / 3;
-    tempInternal1 = tempInternal2;
-    tempInternal2 = sensorValue1;
-
-    // lit la température du DS18B20
-    sensors.requestTemperatures(); 
-    sensorValue5 = sensors.getTempCByIndex(0);
-}
-
-
-
-// Data wire is plugged TO GPIO 4
-#define ONE_WIRE_BUS 4
 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-OneWire oneWire(ONE_WIRE_BUS);
+OneWire oneWire(oneWireBus);
 
 // Pass our oneWire reference to Dallas Temperature. 
 DallasTemperature sensors(&oneWire);
@@ -68,13 +26,26 @@ int numberOfDevices;
 // We'll use this variable to store a found device address
 DeviceAddress tempDeviceAddress; 
 
-void setup(){
-  // start serial port
-  Serial.begin(115200);
-  
-  // Start up the library
+
+// function to print a device address
+void printAddress(DeviceAddress deviceAddress) {
+  for (uint8_t i = 0; i < 8; i++){
+    if (deviceAddress[i] < 16) Serial.print("0");
+      Serial.print(deviceAddress[i], HEX);
+  }
+}
+
+
+// Temperature sensor DS18B20 initialising
+void initDS18B20Sensor(){
+  pinMode(gndPin, OUTPUT);   // gnd
+  digitalWrite(gndPin, LOW);
+  pinMode(pullupPin, INPUT_PULLUP);   // pull up
+  pinMode(vccPin, OUTPUT );   // vcc
+  digitalWrite(vccPin, HIGH);
+  // Start the DS18B20 sensor
   sensors.begin();
-  
+
   // Grab a count of devices on the wire
   numberOfDevices = sensors.getDeviceCount();
   
@@ -99,11 +70,13 @@ void setup(){
       Serial.print(" but could not detect address. Check power and cabling");
     }
   }
+
 }
 
-void loop(){ 
+
+// Lit les senseurs
+void readSensor(){
   sensors.requestTemperatures(); // Send the command to get temperatures
-  
   // Loop through each device, print out temperature data
   for(int i=0;i<numberOfDevices; i++){
     // Search the wire for address
@@ -120,13 +93,5 @@ void loop(){
     }
   }
   delay(5000);
-}
-
-// function to print a device address
-void printAddress(DeviceAddress deviceAddress) {
-  for (uint8_t i = 0; i < 8; i++){
-    if (deviceAddress[i] < 16) Serial.print("0");
-      Serial.print(deviceAddress[i], HEX);
-  }
 }
 
